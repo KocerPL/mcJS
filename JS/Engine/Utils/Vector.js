48aeb0dbce5485b554f343f4b256eb0570d4a1e7
Kocer <kacpersaletnik@op.pl>
2022-01-13 10:18:29 +0000

    Restarted project in typescript

--- JS/Engine/Utils/Vector.js
+++
@@ -1,9 +0,0 @@
-export class Vector
-{
-    constructor(x,y,z)
-    {
-        this.x= x;
-        this.y = y;
-        this.z = z;
-    }
-}
---
+++ TS/Main.ts
@@ -0,0 +1,11 @@
+ class Main
+{
+   public static run():void
+   {
+       let out = document.createElement("output");
+       out.value = "okok";
+       out.style.color = "white";
+        document.body.appendChild(out);
+   }
+}
+Main.run();
